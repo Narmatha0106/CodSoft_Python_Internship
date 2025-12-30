9d4bedc5d52a0e040e10f2280b6c9a9d5dc6a1aa
Narmatha0106 <narmathacse2007@gmail.com>
2025-12-30 17:03:05 +0530

    Add files via upload

---
+++ ToDoList.py
@@ -0,0 +1,36 @@
+tasks=[]
+def show_menu():
+    print("\n____TO-DO LIST MENU____")
+    print("1.Add Task")
+    print("2.View Tasks")
+    print("3.Delete Task")
+    print("4.Exit")
+while True:
+    show_menu()
+    choice=input("Enter your choice(1-4):")
+    if(choice=="1"):
+        task=input("Enter task:")
+        tasks.append(task)
+        print("Task added successfully!")
+    elif(choice=="2"):
+        if not tasks:
+            print("No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            for i,task in enumerate(tasks,start=1):
+                print(f"{i}.{task}")
+    elif choice=="3":
+        if not tasks:
+           print("No tasks to delete")
+        else:
+            task_no=int(input("Enter task number to delete:"))
+            if 1<=task_no<=len(tasks):
+                removed=tasks.pop(task_no-1)
+                print(f"Task'{removed}'deleted.")
+            else:
+                print("Invalid task number.")
+    elif choice=="4":
+        print("Exiting To-Do List.Thank You!")
+        break
+    else:
+        print("Invalid choice.Please try again.")
